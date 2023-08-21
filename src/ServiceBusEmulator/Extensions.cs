3f070d97036ec796277558f23709043c9e6a07d8
Jeffrey Becker <jeffrey.becker@stellarelements.com>
2023-08-21 13:43:03 -0500

    clenaup

--- src/ServiceBusEmulator/Extensions.cs
+++ src/ServiceBusEmulator/Extensions.cs
@@ -18,26 +18,9 @@ namespace ServiceBusEmulator
 
 
             _ = services.AddTransient<ISecurityContext>(sp => SecurityContext.Default);
-
             _ = services.AddTransient<CbsRequestProcessor>();
             _ = services.AddTransient<ITokenValidator>(sp => CbsTokenValidator.Default);
-
-            _ = services.AddOptions<ServiceBusEmulatorOptions>().Configure(configure).PostConfigure<IServerCertificateFactory>((options, certFactory) =>
-            {
-                options.CertificateFactory = certFactory;
-                //if (!string.IsNullOrEmpty(options.ServerCertificateThumbprint))
-                //{
-                //    using X509Store store = new(StoreName.My, StoreLocation.CurrentUser);
-                //    store.Open(OpenFlags.ReadOnly);
-                //    options.ServerCertificate = store.Certificates.Find(X509FindType.FindByThumbprint, options.ServerCertificateThumbprint, false).FirstOrDefault();
-                //}
-
-                //if (!string.IsNullOrEmpty(options.ServerCertificatePath))
-                //{
-                //    options.ServerCertificate = new X509Certificate2(options.ServerCertificatePath, options.ServerCertificatePassword, X509KeyStorageFlags.Exportable);
-                //}
-            }).BindConfiguration("Emulator"); ;
-
+            _ = services.AddOptions<ServiceBusEmulatorOptions>().Configure(configure).BindConfiguration("Emulator");
             _ = services.AddTransient<ServiceBusEmulatorHost>();
             _ = services.AddSingleton<IHostedService, ServiceBusEmulatorWorker>();
 
